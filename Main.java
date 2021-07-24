9df787cf12d9d847f0101d04be4ec35a88ab0ffb
David Elbakian <elbakiand@gmail.com>
2021-07-24 14:27:08 +0400

    javasyntaxpro8l4

---
+++ Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasyntaxpro8l4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David-pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* 1
+        Используя оператор new в методе main(), создай:
+        10 объектов типа String;
+        5 объектов типа int[] (массивов значений int);
+        2 объекта типа Scanner.
+        */
+        
+        String str1 = new String();
+        String str2 = new String();
+        String str3 = new String();
+        String str4 = new String();
+        String str5 = new String();
+        String str6 = new String();
+        String str7 = new String();
+        String str8 = new String();
+        String str9 = new String();
+        String str10 = new String();
+
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[15];
+        int[] arr3 = new int[35];
+        int[] arr4 = new int[51];
+        int[] arr5 = new int[50];
+
+        Scanner console = new Scanner(System.in);
+        Scanner scanner = new Scanner("");      
+      
+    }
+    
+}
